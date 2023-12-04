2fafa078588d2733789037de305e62887bf0ebbb
YossefMagdy3 <yossefshebl3@gmail.com>
2023-12-04 17:30:31 +0200

    to rezk

--- shebl/test1.cpp
+++ shebl/test1.cpp
@@ -6,11 +6,57 @@
 #include <queue>
 #include <list>
 #include <forward_list>
+#include "tree.cpp"
 
 using namespace std;
+// int main()
+// {
+    
+//     ifstream _xml_file("C:\\Users\\youssef\\Desktop\\programing\\DS_c++\\Project\\sample.xml");
+
+//     string _xml_data,tmp;
+//     if( _xml_file.is_open())
+//     {
+//         while (getline(_xml_file,tmp))
+//         {
+           
+//             _xml_data=_xml_data+tmp; //_xml_data.append(tmp);
+//         }
+//     }
+//     //cout<<_xml_data;
+//     //_xml_data.erase(std::remove_if(_xml_data.begin(), _xml_data.end(), "\t"), _xml_data.end());
+    
+//     // regex _tag_regex("<([^<>]+)>");
+//     // sregex_iterator _tag_iterator(_xml_data.begin(),_xml_data.end(),_tag_regex);
+//     // sregex_iterator _endtag_iterator; 
+   
+//     // //"\\(([^)]+)\\)"
+//     // XML_Tree tree;
+//     // bool flag=false;
+//     // while (_tag_iterator !=_endtag_iterator ) {
+//     //     std::smatch match = *_tag_iterator;
+//     //     //Here
+       
+//     //     cout<<match.str()<<endl;
+//     //     ++_tag_iterator;
+//     // }
+
+
+    
+//     regex _info_regex( "<(topic|id|name|body)>(.*?)</(topic|id|name|body)>" );
+
+//     sregex_iterator _info_iterator(_xml_data.begin(),_xml_data.end(),_info_regex);
+//     sregex_iterator _endinfo_iterator; 
+//     while (_info_iterator !=_endinfo_iterator ) {
+//         std::smatch match = *_info_iterator;
+//         cout<<match.str()<<endl;
+//         ++_info_iterator;
+//     }
+
+//     return 0;
+// }
 int main()
 {
-    
     ifstream _xml_file("C:\\Users\\youssef\\Desktop\\programing\\DS_c++\\Project\\sample.xml");
 
     string _xml_data,tmp;
@@ -18,34 +64,48 @@ int main()
     {
         while (getline(_xml_file,tmp))
         {
-           
-            _xml_data=_xml_data+tmp; //_xml_data.append(tmp);
+            _xml_data=_xml_data+tmp+'\n';
         }
     }
-    //_xml_data.erase(std::remove_if(_xml_data.begin(), _xml_data.end(), "\t"), _xml_data.end());
-    
+
+    XML_Tree tree;
+    TreeNode* currentNode = nullptr;
+
     regex _tag_regex("<([^<>]+)>");
     sregex_iterator _tag_iterator(_xml_data.begin(),_xml_data.end(),_tag_regex);
     sregex_iterator _endtag_iterator; 
-   
-    //"\\(([^)]+)\\)"
+
     while (_tag_iterator !=_endtag_iterator ) {
         std::smatch match = *_tag_iterator;
-        //Here
-        cout<<match.str()<<endl;
+        string tag = match.str(1);
+
+        // Check if this is a start tag or an end tag
+        if(tag[1] != '/') // Start tag
+        {
+            string tag_name = tag.substr(1, tag.size() - 2); // Remove the angle brackets
+
+            if(currentNode == nullptr) // If there is no current node, this is the root
+            {
+                tree.InsertRoot(tag, "");
+                currentNode = tree.root;
+            }
+            else // Otherwise, this is a child node
+            {
+                tree.InsertChild(currentNode, tag, "");
+                currentNode = currentNode->children.back();
+            }
+        }
+        else // End tag
+        {
+            currentNode = currentNode->parent; // Move up to the parent node
+        }
+
         ++_tag_iterator;
     }
 
-    
-    // regex _info_regex( "<(topic|id|name|body)>(.*?)</(topic|id|name|body)>" );
-
-    // sregex_iterator _info_iterator(_xml_data.begin(),_xml_data.end(),_info_regex);
-    // sregex_iterator _endinfo_iterator; 
-    // while (_info_iterator !=_endinfo_iterator ) {
-    //     std::smatch match = *_info_iterator;
-    //     cout<<match.str()<<endl;
-    //     ++_info_iterator;
-    // }
+    // Now you can use your tree
+    // For example, print the tree using pre-order traversal
+    tree.preOrderTraversal(tree.root);
 
     return 0;
 }
--- shebl/tree.cpp
+++ shebl/tree.cpp
@@ -6,21 +6,98 @@
 using namespace std;
 
 
+// class TreeNode
+// {
+//      public:
+//     /*datatypes of the tree*/
+//     string _tag_name;
+//     string _tag_data;
+    
+//     /*children of a node *children tags* */
+//     vector<TreeNode*> children ;
+
+
+//     friend class XML_Tree;
+
+// };
+// class XML_Tree
+// {
+
+    
+
+//     public:
+//     TreeNode * root=nullptr;
+    
+//     TreeNode* GetNewNode(string _tag_name , string _tag_data)
+//     {
+//         TreeNode* newNode= new TreeNode();
+//         newNode->_tag_name=_tag_name;
+//         newNode->_tag_data=_tag_data;
+//         return newNode;
+//     }
+//     TreeNode* Insert(TreeNode* root,string _tag_name , string _tag_data)
+//     {
+//         if(root==nullptr)
+//         {
+//             root=GetNewNode(_tag_name,_tag_data);
+//             return root;
+//         }
+//     }
+    
+//     void InsertRoot(string _tag_name , string _tag_data)
+//     {
+//         if(root == nullptr)
+//         {
+//             root = GetNewNode(_tag_name, _tag_data);
+//         }
+//         else
+//         {
+//             cout << "Root already exists." << endl;
+//         }
+//     }
+    
+//     void InsertChild(TreeNode* parent, string _tag_name , string _tag_data)
+//     {
+//         TreeNode* childNode = GetNewNode(_tag_name, _tag_data);
+//         parent->children.push_back(childNode);
+//     }
+    
+//     void preOrderTraversal(TreeNode *node)
+//     {
+//         if(node == nullptr)
+//             return;
+        
+//         //Edit Here -------------------------------- ----------------------------------------------------
+//         cout << node->_tag_name << ": " << node->_tag_data << endl;
+        
+//         for(auto child : node->children)
+//         {
+//             preOrderTraversal(child);
+//         }
+//     }
+// };
+
+
+
 class TreeNode
 {
+public:
     /*datatypes of the tree*/
     string _tag_name;
     string _tag_data;
     
     /*children of a node *children tags* */
-    vector<TreeNode*> children ;
+    vector<TreeNode*> children;
 
+    /*parent of a node*/
+    TreeNode* parent = nullptr;
 
     friend class XML_Tree;
-
 };
+
 class XML_Tree
 {
+public:
     TreeNode * root=nullptr;
     
     TreeNode* GetNewNode(string _tag_name , string _tag_data)
@@ -46,6 +123,7 @@ class XML_Tree
     void InsertChild(TreeNode* parent, string _tag_name , string _tag_data)
     {
         TreeNode* childNode = GetNewNode(_tag_name, _tag_data);
+        childNode->parent = parent; // Set the parent of the child node
         parent->children.push_back(childNode);
     }
     
@@ -54,7 +132,6 @@ class XML_Tree
         if(node == nullptr)
             return;
         
-        //Edit Here -------------------------------- ----------------------------------------------------
         cout << node->_tag_name << ": " << node->_tag_data << endl;
         
         for(auto child : node->children)
@@ -67,16 +144,3 @@ class XML_Tree
 
 
 
- 
-    // void postOrderTraversal(TreeNode *node)
-    // {
-    //     if(node == nullptr)
-    //         return;
-        
-    //     for(auto child : node->children)
-    //     {
-    //         postOrderTraversal(child);
-    //     }
-        
-    //     cout << node->_tag_name << ": " << node->_tag_data << endl;
-    // }
