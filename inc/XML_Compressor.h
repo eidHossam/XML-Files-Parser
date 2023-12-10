2139178ac0908a9351885a364a436e222726e9ae
keroles shenouda <fawzybashandy@gmail.com>
2023-12-10 02:27:15 +0200

    Updating XML_Compressor Header File

--- inc/XML_Compressor.h
+++ inc/XML_Compressor.h
@@ -1,18 +1,13 @@
 #ifndef XML_COMPRESSOR_H_
 #define XML_COMPRESSOR_H_
 
+#include "Node.h"
 #include <iostream>
 #include <unordered_map>
 #include <queue>
 #include <string>
 
 using namespace std;
-
-struct Node {
-    char ch;
-    int freq;
-    Node* left, * right;
-};
 
 struct comp {
     bool operator()(Node* l, Node* r) {
@@ -22,9 +17,8 @@ struct comp {
 
 Node* getNode(char ch, int freq, Node* left, Node* right);
 void Huffman_Encode(Node* root, string str, unordered_map<char, string>& huffmanCode);
-void Calculate_Frequency_Map(unordered_map<char, int>& freq,string text);
-Node * Create_Huffman_Tree(priority_queue<Node*, vector<Node*>, comp> &PriorityQ , unordered_map<char, int> &freq);
+void Calculate_Frequency_Map(unordered_map<char, int>& freq, string text);
+Node* Create_Huffman_Tree(priority_queue<Node*, vector<Node*>, comp>& PriorityQ, unordered_map<char, int>& freq);
 string Huffman_Compress(string text);
-
 
 #endif
